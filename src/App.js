766e3ba57167813649d9c87e81540a8fcff6dfc6
dhiman <gurpreetkumarmail@gmail.com>
2023-08-31 23:30:32 +0530

    base setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './Components/NavBar/NavBar.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div >
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navbar.css";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo">The Hindustan E-Com Shop</div>
+        <div className="nav-actions">Menu</div>
+      </div>
+    </>
+  );
+}
