0aba655cc997ece52d8062b9a287cd8d887cbe77
dhiman <gurpreetkumarmail@gmail.com>
2023-09-22 22:53:48 +0530

    navbar started

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import "./navbar.css";
+import SearchBox from "../CommonComponents/SearchBox.tsx";
 
 export default function NavBar() {
   return (
     <>
       <div className="navbar">
         <div className="logo">The Hindustan E-Com Shop</div>
+        <div>
+          <SearchBox
+            width={200}
+            height={40}
+            label="1213"
+            placeHolder="GlobalSearch"
+          />
+        </div>
         <div className="nav-actions">Menu</div>
       </div>
     </>
